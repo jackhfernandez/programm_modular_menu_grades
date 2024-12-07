39b47d4861b40205e46d96c50fa32ee36ea8e827
Jack <132464970+jackhfernandez@users.noreply.github.com>
2024-12-07 01:03:54 -0500

    Agregando el menu

--- main.cpp
+++ main.cpp
@@ -6,8 +6,32 @@
 
 #include <iostream>
 using namespace std;
+
+void menu();
+void ingresar_opcion(int &op);
+
 int main() {
-  
+
+  string lista_alumnos[5] = {"Pedro", "Juan", "Arturo", "Alisson"};
+  float nota[5] = {8.5, 9.2, 5, 10 };
+
+  menu();
+  int a;
+  ingresar_opcion(a);
+  cout <<"\t"<< a;
 
   return 0;
+}
+
+void menu() {
+  cout<<"\tMenu: \n"<<endl;
+  cout<<"\t1. Lista de alumnos"<<endl;
+  cout<<"\t2. Mostrar nota de alumnos"<<endl;
+  cout<<"\t3. Modificar nota"<<endl;
+  cout<<"\t4. Salir"<<endl;
+}
+
+void ingresar_opcion(int &op) {
+  cout<<"\n\tElija una opcion: ";
+  cin >>op;
 }
