b222d8cfc0da538be160ab27a456308fdc224af7
Jack <132464970+jackhfernandez@users.noreply.github.com>
2024-12-06 23:43:25 -0500

    Creando proyecto

---
+++ main.cpp
@@ -0,0 +1,13 @@
+/* Mostrar un menu que muestre de forma repetitiva lo siguiente
+ - Mostar en pantalla el nombre de los alumnos
+ - Mostra la nota de un alumno especifico
+ - Modificar la nota pidiendo el nombre
+*/
+
+#include <iostream>
+using namespace std;
+int main() {
+  
+
+  return 0;
+}
